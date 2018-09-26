f2c7ecf23ca4bde39cf4bb9b90146bb25cac291c
santosh131 <santosh_janapala@hotmail.com>
2018-09-26 03:13:33 -0400

    second commit

--- CollegeAdministration/Controllers/HomeController.cs
+++ CollegeAdministration/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CollegeAdministration.Infrastructure;
+using CollegeAdministration.Models;
 using CollegeAdministration.Providers;
 
 namespace CollegeAdministration.Controllers
@@ -14,8 +15,9 @@ namespace CollegeAdministration.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
-            return View();
+            List<Student> lstStudents = new List<Student>();
+            lstStudents.Add(new Student() { StudentId=1, Name="Samuel", ActiveInd=true, Address="123 Ljj Ave", Mobile="121313" });
+            return View(lstStudents);
         }
     }
 }
---
+++ CollegeAdministration/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using CollegeAdministration.Models;
+using CollegeAdministration.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeAdministration.Controllers
+{
+    [CustomAuthorization]
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> lstStudents = new List<Student>();
+            lstStudents.Add(new Student() { StudentId = 1, Name = "Samuel", ActiveInd = true, Address = "123 NY Ave", Mobile = "123456789" });
+            return View(lstStudents);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
